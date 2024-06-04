56dd2b377c28cb74e3c90032e7c15e2250cb6432
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2024-06-04 16:39:00 +0800

    Fixed typo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from "../assets/svg/ButtonSvg.jsx";
 
 const Button = ({className, href, onClick, children, px, white}) => {
-    const classes = `button relative inline-flex items-center justify-center h-11 transition colours text-n-1/50
+    const classes = `button relative inline-flex items-center justify-center h-11 transition-colors text-n-1/50
     hover:text-color-7 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
     const spanClasses = 'relative z-10';
